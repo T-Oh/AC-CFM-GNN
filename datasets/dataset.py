f1c7a8ec314b54d3d5e8d625b911e6f31469d42a
T-Oh <75983560+T-Oh@users.noreply.github.com>
2023-03-29 18:44:19 +0200

    Added calc_mask_probs()
    For now calculates the variance of node_labels per node

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -540,3 +540,25 @@ def create_loaders(cfg, trainset, testset, pre_compute_mean=False):
     #logging.debug(f"Testset first batch labels: {next(iter(testloader)).y.tolist()}")
 
     return trainloader, testloader
+
+
+def calc_mask_probs(dataloader):
+    node_label_means = torch.zeros(2000)
+    node_label_vars = torch.zeros(2000)
+    for i, batch in enumerate(dataloader):
+        batchsize = int(len(batch.node_labels)/2000)
+        for k in range(batchsize):
+            for j in range(2000):
+                node_label_means[j] += batch.node_labels[j+k*2000]
+                if (j+k*2000)%2000 == 0:
+                    print(batch.node_labels[j+k*2000])
+    print(node_label_means)
+    node_label_means /= (i+1)*batchsize
+    print(node_label_means)
+    for i, batch in enumerate(dataloader):
+        batchsize = int(len(batch.node_labels)/2000)
+        for k in range(batchsize):
+            for j in range(2000):
+                node_label_vars[j] += (batch.node_labels[j+k*2000]-node_label_means[j])**2
+    node_label_vars = torch.sqrt(node_label_vars/((i+1)*batchsize))
+    print(node_label_vars[0])
