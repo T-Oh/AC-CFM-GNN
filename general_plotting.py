88b73a55b0776430435778e99a93459a4abf6999
T-Oh <75983560+T-Oh@users.noreply.github.com>
2023-05-08 10:49:10 +0200

    added plot_step_outage

--- general_plotting.py
+++ general_plotting.py
@@ -12,12 +12,12 @@ import os
 
 #Data
 #hists=np.load('correct_node_labels_histograms.npz')
-path='processed/'
+"""path='processed/'
 for file in os.listdir(path):
     data=torch.load(path+file)['x']
     x=np.arange(len(data[:,0]))
     fig1,ax1=plt.subplots()
-    ax1.plot(x,data[:,0],x,data[:,1])
+    ax1.plot(x,data[:,0],x,data[:,1])"""
 
     
 """bins=hists['node_label_bins']
@@ -34,3 +34,20 @@ ax1.set_xlabel("Power Outage")
 #fig1.savefig("local_subset_node_feature_distr_power.png")
 """
 
+
+def plot_step_outage(file=None, name=None):
+    if name == None:
+        name = 'Ike_ajusted'
+    if file == None:
+        data = np.load('C:/Users/tobia/OneDrive/Dokumente/Master/Semester4/Masterarbeit/results/cluster/AC_Feature_Distr/Ike_ajusted/total_step_outage_distribution_Ike_ajusted.npz')
+    else:
+        data = np.load(file)
+    node_label_bins=data['node_label_bins']
+    node_label_hist=data['node_label_hist']
+    fig5,ax5=plt.subplots()
+    ax5.bar(node_label_bins[0:10],node_label_hist,width=node_label_bins[1]-node_label_bins[0],align='edge')
+    ax5.set_title(name)
+    ax5.set_xlabel("Total Step Outage")
+    ax5.set_ylabel("Count")
+    ax5.set_ylim(0,4000)
+    #fig5.savefig("Total_Step_Outage_Distribution_"+name+'png')
