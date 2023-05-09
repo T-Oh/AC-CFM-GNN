73f54f2c9de14e3a0f15b9308d38cb44c6c776f8
T-Oh <75983560+T-Oh@users.noreply.github.com>
2023-05-09 16:48:03 +0200

    Changes to correctly display values of BayesOpt
    - no casts int values to int since BayesOpt returns floats
    - added name variable to control naming of plots

--- analyze_ray.py
+++ analyze_ray.py
@@ -7,10 +7,11 @@ import json
 import matplotlib.pyplot as plt
 import torch
 import numpy as np
+from training.training import objective
 
 ray.init()
-path = '/p/tmp/tobiasoh/machine_learning/results/GINE/4000/study/objective_2023-03-28_13-29-27/'
-tuner = tune.Tuner.restore(path)
+path = '/p/tmp/tobiasoh/machine_learning/ajusted_subset_alllog/results/objective_2023-05-07_10-30-45/'
+tuner = tune.Tuner.restore(path, objective)
 result_grid=tuner.get_results()
 R2 = []
 DM = []
@@ -23,49 +24,63 @@ gc = []
 skipcon = []
 RHS = []
 RHL = []
-print(result_grid.get_best_result())
-for i in range(20):
+mask_bias = []
+loss_weight = []
+#print(result_grid.get_best_result())
+for i in range(26):
     print(i)
+    print(result_grid[i].error)
+    #print(result_grid[i].metrics['r2'])
     if not result_grid[i].error:
-        if result_grid[i].metrics['r2'] < -1e33:
-            continue
-        DM.append(result_grid[i].metrics['discrete_measure'])
-        R2.append(result_grid[i].metrics['r2'])
-        with open(result_grid[i].log_dir/'params.json', 'r') as f:
-           data = json.load(f)
-        LR.append(data['LR'])
-        print(type(data['LR']))
-        
-        layers.append(data['layers'])
-        HF.append(data['HF'])
-        gc.append(data['gradclip'])
-        dropout.append(data['dropout'])
-        skipcon.append(data['use_skipcon'])
-        RHS.append(data['reghead_size'])
-        RHL.append(data['reghead_layers'])
+        if 'r2' in result_grid[i].metrics.keys():
+            if result_grid[i].metrics['r2'] < -1e33:
+                continue
+            DM.append(result_grid[i].metrics['discrete_measure'])
+            R2.append(result_grid[i].metrics['r2'])
+            with open(result_grid[i].log_dir/'params.json', 'r') as f:
+                data = json.load(f)
+            LR.append(data['LR'])
+            print(type(data['LR']))
+    
+            layers.append(int(data['layers']))
+            HF.append(int(data['HF']))
+            gc.append(data['gradclip'])
+            dropout.append(data['dropout'])
+            skipcon.append(int(data['use_skipcon']))
+            RHS.append(int(data['reghead_size']))
+            RHL.append(int(data['reghead_layers']))
+            mask_bias.append(data['mask_bias'])
+            loss_weight.append(data['loss_weight'])
+        else: print(f'Skipped i')
 
-path = '/p/tmp/tobiasoh/machine_learning/NodeRegression/results/objective_2023-03-29_09-15-15/'
-tuner = tune.Tuner.restore(path)
+path = '/p/tmp/tobiasoh/machine_learning/ajusted_subset_alllog/results/objective_2023-05-08_08-27-20/'
+tuner = tune.Tuner.restore(path, objective)
 result_grid=tuner.get_results()
-for i in range(20):
+for i in range(26):
     print(i)
+    print(result_grid[i].error)
+    #print(result_grid[i].metrics['r2'])
     if not result_grid[i].error:
-        if result_grid[i].metrics['r2'] < -1e33:
-            continue
-        DM.append(result_grid[i].metrics['discrete_measure'])
-        R2.append(result_grid[i].metrics['r2'])
-        with open(result_grid[i].log_dir/'params.json', 'r') as f:
-           data = json.load(f)
-        LR.append(data['LR'])
-        print(type(data['LR']))
+        if 'r2' in result_grid[i].metrics.keys():
+            if result_grid[i].metrics['r2'] < -1e33:
+                continue
+            DM.append(result_grid[i].metrics['discrete_measure'])
+            R2.append(result_grid[i].metrics['r2'])
+            with open(result_grid[i].log_dir/'params.json', 'r') as f:
+                data = json.load(f)
+            LR.append(data['LR'])
+            print(type(data['LR']))
 
-        layers.append(data['layers'])
-        HF.append(data['HF'])
-        gc.append(data['gradclip'])
-        dropout.append(data['dropout'])
-        skipcon.append(data['use_skipcon'])
-        RHS.append(data['reghead_size'])
-        RHL.append(data['reghead_layers'])
+            layers.append(int(data['layers']))
+            HF.append(int(data['HF']))
+            gc.append(data['gradclip'])
+            dropout.append(data['dropout'])
+            skipcon.append(int(data['use_skipcon']))
+            RHS.append(int(data['reghead_size']))
+            RHL.append(int(data['reghead_layers']))
+            mask_bias.append(data['mask_bias'])
+            loss_weight.append(data['loss_weight'])
+        else: print(f'Skipped i')
 
 
 
@@ -75,19 +90,22 @@ print(R2)
 print(DM)
 print(gc)
 
+name = 'lognorm'
+
 fig1,ax1=plt.subplots()
 ax1.scatter(gc,R2)
 ax1.set_title("")
-ax1.set_xlabel("Norm for gradient clipping")
+#ax1.set_xscale("log")
+ax1.set_xlabel("Gradclip")
 ax1.set_ylabel('R2')
-fig1.savefig("GC_R2_50_4000.png")
+fig1.savefig("GC_R2_25_" + name + ".png")
 
 fig2,ax2=plt.subplots()
-ax2.scatter(np.array(HF),np.array(DM))
+ax2.scatter(np.array(HF),np.array(R2))
 ax2.set_title("")
 ax2.set_xlabel("N Hidden Features")
-ax2.set_ylabel('Discrete Measure')
-fig2.savefig("HF_DM_50_4000.png")
+ax2.set_ylabel('R2')
+fig2.savefig("HF_R2_25_" + name + ".png")
 
 fig3,ax3=plt.subplots()
 ax3.scatter(LR,R2)
@@ -95,42 +113,42 @@ ax3.set_title("")
 ax3.set_xscale("log");
 ax3.set_xlabel("Learning Rate")
 ax3.set_ylabel('R2')
-fig3.savefig("LR_R2_50_4000.png")
+fig3.savefig("LR_R2_25_" + name + ".png")
 
 fig4,ax4=plt.subplots()
 ax4.scatter(layers,R2)
 ax4.set_title("")
 ax4.set_xlabel("N Layers")
 ax4.set_ylabel('R2')
-fig4.savefig("layers_R2_50_4000.png")
+fig4.savefig("layers_R2_25_" + name + ".png")
 
 fig5,ax5=plt.subplots()
 ax5.scatter(skipcon,R2)
 ax5.set_title("")
 ax5.set_xlabel("use_skipcon")
 ax5.set_ylabel('R2')
-fig5.savefig("skipcon_R2_50_4000.png")
+fig5.savefig("skipcon_R2_25_" + name + ".png")
 
 fig7,ax7=plt.subplots()
-ax7.scatter(dropout,layers,c=R2)
+ax7.scatter(dropout,R2)
 ax7.set_title("")
 ax7.set_xlabel("dropout")
 ax7.set_ylabel('R2')
-fig7.savefig("dropout_layers_cR2_50_4000.png")
+fig7.savefig("dropout_cR2_25_" + name + ".png")
 
 fig9,ax9=plt.subplots()
-ax9.scatter(RHS,R2,c=RHL)
+ax9.scatter(RHS,R2)
 ax9.set_title("")
 ax9.set_xlabel("Reghead Hidden Features")
 ax9.set_ylabel('R2')
-fig9.savefig("RHsize_R2_cRHL_750_4000.png")
+fig9.savefig("RHsize_R2_25_" + name + ".png")
 
 fig8,ax8=plt.subplots()
-ax8.scatter(RHL,R2,c=RHS)
+ax8.scatter(RHL,R2)
 ax8.set_title("")
 ax8.set_xlabel("RegHead Layers")
 ax8.set_ylabel('R2')
-fig8.savefig("RHL_R2_cRHS_50_4000.png")
+fig8.savefig("RHL_R2_25_" + name + ".png")
 
 
 fig6 = plt.figure()
@@ -140,16 +158,16 @@ ax6.set_title("")
 ax6.set_xlabel("dropout")
 ax6.set_ylabel('N Layers')
 ax6.set_zlabel('R2')
-fig6.savefig("dropout_layers_R2_50_1500.png")
+fig6.savefig("dropout_layers_R2_25_" + name + ".png")
 
 fig6 = plt.figure()
 ax6 = fig6.add_subplot(projection='3d')
 ax6.scatter(gc,HF, R2,c=R2)
 ax6.set_title("")
-ax6.set_xlabel("N Heads")
+ax6.set_xlabel("Gradclip Norm")
 ax6.set_ylabel('N HF')
 ax6.set_zlabel('R2')
-fig6.savefig("GC_HF_R2_50_1500.png")
+fig6.savefig("GC_HF_R2_25_" + name + ".png")
 
 fig6 = plt.figure()
 ax6 = fig6.add_subplot(projection='3d')
@@ -158,4 +176,13 @@ ax6.set_title("")
 ax6.set_xlabel("N HF")
 ax6.set_ylabel('N Layers')
 ax6.set_zlabel('R2')
-fig6.savefig("HF_layers_R2_50_1500.png")
+fig6.savefig("HF_layers_R2_25_" + name + ".png")
+
+fig6 = plt.figure()
+ax6 = fig6.add_subplot(projection='3d')
+ax6.scatter(RHL,RHS, R2,c=R2)
+ax6.set_title("")
+ax6.set_xlabel("N Reghead Layers")
+ax6.set_ylabel('N Reghead Features')
+ax6.set_zlabel('R2')
+fig6.savefig("RHL_RHS_R2_25_" + name + ".png")
