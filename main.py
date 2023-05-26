ab976f40bc38897fe4c834134adb7ad199952287
T-Oh <75983560+T-Oh@users.noreply.github.com>
2023-05-26 16:37:57 +0200

    Moved dataset creation into run functions

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import shutil
 import time
 
 from numpy.random import seed as numpy_seed
-from os.path import isfile
 
-from datasets.dataset import create_datasets, create_loaders, calc_mask_probs
 from run.run_single import run_single
 from run.run_crossval import run_crossval
 from run.run_study import run_study
@@ -31,35 +29,7 @@ assert not (cfg['crossvalidation'] and cfg['study::run']), 'can only run a study
 shutil.copyfile("configurations/configuration.json", "results/configuration.json")
 logging.basicConfig(filename=cfg['dataset::path'] + "results/regression.log", filemode="w", level=logging.INFO)
 
-# Create Datasets and Dataloaders
-print('Creating Datasets...')
-trainset, testset, data_list = create_datasets(cfg["dataset::path"], cfg=cfg, pre_transform=None, stormsplit=cfg['stormsplit'])
-t2 = time.time()
-print(f'Creating datasets took {(start-t2)/60} mins')
-print('Creating Dataloaders...')
-trainloader, testloader = create_loaders(cfg, trainset, testset)
-print(f'Creating dataloaders took {(t2-time.time())/60} mins')
 
-
-
-# Calculate probabilities for masking of nodes if necessary
-if cfg['use_masking'] or cfg['weighted_loss_var'] or (cfg['study::run'] and (cfg['study::masking'] or cfg['study::loss_type'])):
-    if isfile('node_label_vars.pt'):
-        print('Using existing Node Label Variances for masking')
-        mask_probs = torch.load('node_label_vars.pt')
-    else:
-        print('No node label variance file found\nCalculating Node Variances for Masking')
-        mask_probs = calc_mask_probs(trainloader)
-        torch.save(mask_probs, 'node_label_vars.pt')
-else:
-    #Masks are set to one in case it is wrongly used somewhere (when set to 1 masking results in multiplication with 1)
-    mask_probs = torch.zeros(2000)+1
-
-
-# getting feature and target sizes
-num_features = trainset.__getitem__(0).x.shape[1]
-num_edge_features = trainset.__getitem__(0).edge_attr.shape[1]
-num_targets = 1
 
 
 # choosing device
@@ -78,15 +48,15 @@ if device == "cuda":
 
 # Runs study if set in configuration file
 if cfg["study::run"]:
-    model = run_study(cfg, trainloader, testloader, device, mask_probs, num_features, num_edge_features)
+    model = run_study(cfg, device)
 
 #Runs crossvalidation
 elif cfg['crossvalidation']:
-    model = run_crossval(cfg, device, mask_probs, num_features, num_edge_features)
+    model = run_crossval(cfg, device)
 
 #Runs a single configuration    
 else:
-    model = run_single(cfg, trainloader, testloader, device, mask_probs, num_features, num_edge_features)
+    model = run_single(cfg, device)
 
 
 
