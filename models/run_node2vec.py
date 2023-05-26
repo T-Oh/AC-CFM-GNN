800e06d431ceec6ca2d384724e54482f638b75c9
T-Oh <75983560+T-Oh@users.noreply.github.com>
2023-05-26 16:41:50 +0200

    run_node2vec checks if node2vec/ exists and creates it if not

--- models/run_node2vec.py
+++ models/run_node2vec.py
@@ -4,14 +4,16 @@ Created on Tue May 23 15:38:58 2023
 
 @author: tobia
 """
-from torch_geometric.nn import Node2Vec
-from utils.get_optimizers import get_optimizer
-from datasets.dataset import save_node2vec
 import torch
+import os
+from torch_geometric.nn import Node2Vec
+
+
+from utils.get_optimizers import get_optimizer
+
 
 def run_node2vec(cfg, trainloader, device, params, trial):
     edge_index = next(iter(trainloader)).edge_index
-    labels = next(iter(trainloader)).node_labels
     model = Node2Vec(edge_index,
                      embedding_dim = params['embedding_dim'],
                      walk_length = params['walk_length'],
@@ -31,13 +33,12 @@ def run_node2vec(cfg, trainloader, device, params, trial):
         #acc = test(model, labels)
         #print(f'Loss: {loss} \nAcc: {acc}')
         print(f'Loss: {loss}')
-        
-    
-        
+               
     embedding = model()
         
             
-    #save_node2vec(embedding, labels, data_list)
+    if not os.path.exists('node2vec/'):
+        os.makedirs('node2vec/')
     torch.save(embedding, f'node2vec/embedding_{trial}.pt')
     return embedding
     
