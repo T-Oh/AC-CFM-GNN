c1f976bda419938fe842ac097db43aa9736c9de3
T-Oh <75983560+T-Oh@users.noreply.github.com>
2023-06-21 15:55:00 +0200

    now takes params as argument and reads LR and weight decay from there

--- utils/get_optimizers.py
+++ utils/get_optimizers.py
@@ -1,6 +1,6 @@
 from torch.optim import SGD, Adam
 
-def get_optimizer(cfg, model):
+def get_optimizer(cfg, model, params):
     """
     Helper function which gets the desired optimizer
     and initializes it
@@ -8,19 +8,19 @@ def get_optimizer(cfg, model):
     if cfg["optim::optimizer"] == "SGD":
         optimizer = SGD(
             model.parameters(),
-            lr = cfg["optim::LR"],
+            lr = params['LR'],
             momentum=cfg["optim::momentum"],
             nesterov=cfg["optim::nesterov"],
-            weight_decay=cfg["optim::weight_decay"]
+            weight_decay=params["weight_decay"]
         )
         print(cfg["optim::LR"])
     elif cfg["optim::optimizer"] == "Adam":
         optimizer = Adam(
             model.parameters(),
-            lr = cfg["optim::LR"],
+            lr = params['LR'],
             betas=cfg["optim::betas"],
-            weight_decay=cfg["optim::weight_decay"],
-            eps = cfg['optim::epsilon']
+            weight_decay=params["weight_decay"],
+            eps = params['optim::epsilon']
         )
     else:
         raise ValueError("Optimizer {} does not exist".format(cfg["optim::optimizer"]))
