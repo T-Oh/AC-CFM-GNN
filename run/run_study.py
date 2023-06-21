750a3ac3b8b14c5803d78fe9165bcf20b175a11d
T-Oh <75983560+T-Oh@users.noreply.github.com>
2023-06-21 15:52:43 +0200

    Added node2vec to run_study

--- run/run_study.py
+++ run/run_study.py
@@ -12,9 +12,10 @@ from ray import tune, air
 from os.path import isfile
 
 from datasets.dataset import create_datasets, create_loaders, calc_mask_probs
-from utils.utils import setup_searchspace
+from utils.utils import setup_searchspace, setup_params
 from ray.tune.search.bayesopt import BayesOptSearch
 from training.training import objective
+from baselines.run_node2vec import run_node2vec
 
 
 
@@ -26,19 +27,20 @@ def run_study(cfg, device, N_CPUS, port_dashboard):
     N_CPUS = N_CPUS
     port_dashboard = port_dashboard
     # init ray
-    ray.init(_temp_dir=TEMP_DIR, num_cpus=N_CPUS, num_gpus=N_GPUS,
+    ray.init( _temp_dir=TEMP_DIR,num_cpus=N_CPUS, num_gpus=N_GPUS,
              include_dashboard=True, dashboard_port=port_dashboard)
     
     # Create Datasets and Dataloaders
     trainset, testset, data_list = create_datasets(cfg["dataset::path"], cfg=cfg, pre_transform=None, stormsplit=cfg['stormsplit'])
     trainloader, testloader = create_loaders(cfg, trainset, testset)
+   
     
     # getting feature and target sizes
     num_features = trainset.__getitem__(0).x.shape[1]
     num_edge_features = trainset.__getitem__(0).edge_attr.shape[1]
 
     # Calculate probabilities for masking of nodes if necessary
-    if cfg['use_masking'] or cfg['weighted_loss_var'] or (cfg['study::run'] and (cfg['study::masking'] or cfg['study::loss_type'])):
+    if cfg['use_masking'] or (cfg['study::run'] and (cfg['study::masking'] or cfg['study::loss_type'])):
         if isfile('node_label_vars.pt'):
             print('Using existing Node Label Variances for masking')
             mask_probs = torch.load('node_label_vars.pt')
@@ -49,6 +51,33 @@ def run_study(cfg, device, N_CPUS, port_dashboard):
     else:
         #Masks are set to one in case it is wrongly used somewhere (when set to 1 masking results in multiplication with 1)
         mask_probs = torch.zeros(2000)+1
+     
+    #Node2Vec
+    params = setup_params(cfg, mask_probs, num_features, num_edge_features)
+    
+    if cfg['model'] == 'Node2Vec':
+        print('Creating Node2Vec Embedding')
+        
+        embedding = run_node2vec(cfg, trainloader, device, params, 0)
+        normalized_embedding = embedding.data
+        #Normalize the Embedding
+        print(embedding.shape)
+        for i in range(embedding.shape[1]):
+            normalized_embedding[:,i] = embedding[:,i].data/embedding[:,i].data.max()
+            
+        # Create Datasets and Dataloaders
+        trainset, testset, data_list = create_datasets(cfg["dataset::path"], cfg=cfg, pre_transform=None, stormsplit=cfg['stormsplit'], embedding=normalized_embedding.to(device))
+        trainloader, testloader = create_loaders(cfg, trainset, testset)
+
+       
+        # getting feature and target sizes
+        num_features = trainset.__getitem__(0).x.shape[1]
+        print(f'New number of features: {num_features}')
+        num_edge_features = trainset.__getitem__(0).edge_attr.shape[1]
+        
+        #Setup params for following task (MLP)
+        params['num_features'] = num_features
+    #Node2Vec End
 
     
     # uses ray to run a study, to see functionality check training.objective
