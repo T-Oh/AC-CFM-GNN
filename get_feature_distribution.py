ce3cb632fd8cada13fc1bf9dae88c9b7b432a5b9
T-Oh <tobias_ohlinger@web.de>
2024-06-18 10:59:17 +0200

    Changed processing in get_feature_distribution.py Can now deal with any edge_feature size compared to the hard_coded 6 edge Features before

--- get_feature_distribution.py
+++ get_feature_distribution.py
@@ -9,10 +9,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-PLOT_ONLY = True
+PLOT_ONLY = False
 NO_Va = True
 path='processed/'
 NAME = 'plotting_test'
+N_NODE_FEATURES = 17    #if NodeIDs are added as features substract 2000 from N_Features
+N_EDGE_FEATURES = 1
+
+
 
 
 def get_hist(data, bins):
@@ -24,15 +28,15 @@ def get_hist(data, bins):
                 break
     return hist
 
-def get_min_max_features(path):
-    x_max = torch.zeros(18)
-    x_min = torch.zeros(18)
-    edge_attr_max = torch.zeros(5)
-    edge_attr_min = torch.zeros(5)
-    for i in range(18):
+def get_min_max_features(path, N_node_features, N_edge_features):
+    x_max = torch.zeros(N_node_features)
+    x_min = torch.zeros(N_node_features)
+    edge_attr_max = torch.zeros(N_edge_features)
+    edge_attr_min = torch.zeros(N_edge_features)
+    for i in range(N_node_features):
         x_max[i] = np.NINF
         x_min[i] = np.Inf
-        if i <5:
+        if i <N_edge_features:
             edge_attr_max[i] =  np.NINF
             edge_attr_min[i] = np.Inf
 
@@ -54,17 +58,16 @@ def get_min_max_features(path):
 
             
         edge_attr=torch.load(path+file)['edge_attr']
-        for i in range(len(edge_attr[:,0])):
-            if edge_attr[i,0] > edge_attr_max[0]: edge_attr_max[0] = edge_attr[i,0]
-            if edge_attr[i,0] < edge_attr_min[0]: edge_attr_min[0] = edge_attr[i,0]
-            if edge_attr[i,1] > edge_attr_max[1]: edge_attr_max[1] = edge_attr[i,1]
-            if edge_attr[i,1] < edge_attr_min[1]: edge_attr_min[1] = edge_attr[i,1]
-            if edge_attr[i,2] > edge_attr_max[2]: edge_attr_max[2] = edge_attr[i,2]
-            if edge_attr[i,2] < edge_attr_min[2]: edge_attr_min[2] = edge_attr[i,2]
-            if edge_attr[i,3] > edge_attr_max[3]: edge_attr_max[3] = edge_attr[i,3]
-            if edge_attr[i,3] < edge_attr_min[3]: edge_attr_min[3] = edge_attr[i,3]
-            if edge_attr[i,4] > edge_attr_max[4]: edge_attr_max[4] = edge_attr[i,4]
-            if edge_attr[i,4] < edge_attr_min[4]: edge_attr_min[4] = edge_attr[i,4]
+        if N_edge_features == 1:
+            for i in range(len(edge_attr)):
+                if edge_attr[i] > edge_attr_max[0]: edge_attr_max[0] = edge_attr[i]
+                if edge_attr[i] < edge_attr_min[0]: edge_attr_min[0] = edge_attr[i]
+        else:
+            for i in range(edge_attr.shape[0]):
+                for j in range(len(edge_attr_max)):
+                    if edge_attr[i,j] > edge_attr_max[j]: edge_attr_max[j] = edge_attr[i,j]
+                    if edge_attr[i,j] < edge_attr_min[j]: edge_attr_min[j] = edge_attr[i,j]
+
             
         if torch.is_tensor(torch.load(path+file)['node_labels']):
             print('TEST')
@@ -84,21 +87,9 @@ if PLOT_ONLY:
     
     x_bins = data['x_bins']
     
-    edgehist1 = data['edgehist1']
-    edgehist2 = data['edgehist2']
-    edgehist3 = data['edgehist3']
-    edgehist4 = data['edgehist4']
-    edgehist5 = data['edgehist5']
-    edgehist6 = data['edgehist6']
-    #edgehist7 = data['edgehist7']
-    
-    edgebins1 = data['edgebins1']
-    edgebins2 = data['edgebins2']
-    edgebins3 = data['edgebins3']
-    edgebins4 = data['edgebins4']
-    edgebins5 = data['edgebins5']
-    edgebins6 = data['edgebins6']
-    #edgebins7 = data['edgebins7']
+    edge_hists = data['edge_hists']
+
+    edge_bins = data['edge_bins']
     
     node_label_hist = data['node_label_hist']
     
@@ -108,9 +99,10 @@ if PLOT_ONLY:
 
         
 else:
-    x_min, x_max, edge_attr_min, edge_attr_max, node_labels_min, node_labels_max = get_min_max_features(path)
+    x_min, x_max, edge_attr_min, edge_attr_max, node_labels_min, node_labels_max = get_min_max_features(path, N_NODE_FEATURES, N_EDGE_FEATURES)
     
     x_bins = np.zeros([len(x_max),10])
+    edge_bins = np.zeros([N_EDGE_FEATURES, 10])
     for i in range(len(x_max)):
         print(i)
         print(x_max[i])
@@ -121,17 +113,14 @@ else:
         #else:
         #    x_bins[i] = np.arange(x_min[i],x_max[i]+x_max[i]/10,(x_max[i]-x_min[i])/9)
         x_bins[i] = np.linspace(x_min[i],x_max[i],10)
-    """x1bins=np.arange(x_min[0],x_max[0]+x_max[0]/10,(x_max[0]-x_min[0])/10)
-    x2bins=np.arange(x_min[1],x_max[1]+x_max[1]/10,(x_max[1]-x_min[1])/10)
-    x3bins=np.arange(x_min[2],x_max[2]+x_max[2]/10,(x_max[2]-x_min[2])/10)
-    x4bins=np.arange(x_min[3],x_max[3]+x_max[3]/10,(x_max[3]-x_min[3])/10)"""
-    edgebins1 = np.arange(edge_attr_min[0],edge_attr_max[0]+edge_attr_max[0]/10,(edge_attr_max[0]-edge_attr_min[0])/10)
-    edgebins2 = np.arange(edge_attr_min[1],edge_attr_max[1]+edge_attr_max[1]/10,(edge_attr_max[1]-edge_attr_min[1])/10)
-    edgebins3 = np.arange(edge_attr_min[2],edge_attr_max[2]+edge_attr_max[2]/10,(edge_attr_max[2]-edge_attr_min[2])/10)
-    #edgebins4 = np.arange(0,1.1,1/10)
-    edgebins4 = np.arange(edge_attr_min[3],edge_attr_max[3]+edge_attr_max[3]/10,(edge_attr_max[3]-edge_attr_min[3])/10)
-    edgebins5 = np.arange(edge_attr_min[4],edge_attr_max[4]+edge_attr_max[4]/10,(edge_attr_max[4]-edge_attr_min[4])/10)
-    edgebins6 = np.arange(0,1.1,1/10)
+    #if N_EDGE_FEATURES == 1:
+     #   edge_bins = np.arange(edge_attr_min[0],edge_attr_max[0]+edge_attr_max[0]/10,(edge_attr_max[0]-edge_attr_min[0])/10)
+
+    #else:
+    for i in range(N_EDGE_FEATURES):
+        edge_bins[i] = np.linspace(edge_attr_min[0],edge_attr_max[0], 10)
+
+        #edgebins6 = np.arange(0,1.1,1/10)
     
     
     #labelbins=np.arange(label_min,label_max,(label_max-label_min)/10)
@@ -140,6 +129,7 @@ else:
     
     first = True
     x_hists = np.zeros([len(x_max),10])
+    edge_hists = np.zeros([N_EDGE_FEATURES,10])
     for file in os.listdir(path):
         if file.startswith('data'):
             data=torch.load(path+file)
@@ -147,18 +137,13 @@ else:
                 
                 for i in range(len(x_max)):
                    x_hists[i] = get_hist(data['x'][:,i],x_bins[i])
-                """x1hist=get_hist(data['x'][:,0],x1bins)
-                x2hist = get_hist(data['x'][:,1],x2bins)
-                x3hist=get_hist(data['x'][:,2],x3bins)
-                x4hist = get_hist(data['x'][:,3],x4bins)"""
 
-                edgehist1 = get_hist(data['edge_attr'][:,0],edgebins1)
-                edgehist2 = get_hist(data['edge_attr'][:,1],edgebins2)
-                edgehist3 = get_hist(data['edge_attr'][:,2],edgebins3)
-                #edgehist4 = get_hist(data['edge_attr'][:,3],edgebins4)
-                edgehist4 = get_hist(data['edge_attr'][:,3],edgebins4)
-                edgehist5 = get_hist(data['edge_attr'][:,4],edgebins5)
-                edgehist6 = get_hist(data['edge_attr'][:,5],edgebins6)
+                if N_EDGE_FEATURES == 1:
+                    edge_hists[0] = get_hist(data['edge_attr'],edge_bins[0])   
+                else:
+                    for i in range(N_EDGE_FEATURES):
+                        edge_hists[i] = get_hist(data['edge_attr'][:,i],edge_bins[i])
+
                 #labelhist = get_hist(data['y'],labelbins)
                 node_label_hist=get_hist(data['node_labels'], node_label_bins)
                 first = False
@@ -166,61 +151,24 @@ else:
                 
                 for i in range(len(x_max)):
                     x_hists[i] += get_hist(data['x'][:,i],x_bins[i])
-                """x2hist_temp = get_hist(data['x'][:,1],x2bins)
-                x3hist_temp=get_hist(data['x'][:,2],x3bins)
-                x4hist_temp = get_hist(data['x'][:,3],x4bins)"""
 
-                edgehist1_temp = get_hist(data['edge_attr'][:,0],edgebins1)
-                edgehist2_temp = get_hist(data['edge_attr'][:,1],edgebins2)
-                edgehist3_temp = get_hist(data['edge_attr'][:,2],edgebins3)
-                edgehist4_temp = get_hist(data['edge_attr'][:,3],edgebins4)
-                edgehist5_temp = get_hist(data['edge_attr'][:,4],edgebins5)
-                edgehist6_temp = get_hist(data['edge_attr'][:,5],edgebins6)
-                #edgehist7_temp = get_hist(data['edge_attr'][:,6],edgebins7)
-                #labelhist_temp = get_hist(data['y'],labelbins)
+                if N_EDGE_FEATURES == 1:
+                    edge_hists[0] += get_hist(data['edge_attr'][:],edge_bins)
+                else:
+                    for i in range(N_EDGE_FEATURES):
+                        edge_hists[i] += get_hist(data['edge_attr'][:,i],edge_bins)
+
                 node_label_hist_temp = get_hist(data['node_labels'],node_label_bins)
-
-                edgehist1 += edgehist1_temp
-                edgehist2 += edgehist2_temp
-                edgehist3 += edgehist3_temp
-                edgehist4 += edgehist4_temp
-                edgehist5 += edgehist5_temp
-                edgehist6 += edgehist6_temp
-                #edgehist7 += edgehist7_temp
-                #labelhist += labelhist_temp
                 node_label_hist += node_label_hist_temp
-    np.savez(NAME,x_hists = x_hists, edgehist1=edgehist1, edgehist2=edgehist2, edgehist3=edgehist3, edgehist4=edgehist4, edgehist5=edgehist5, edgehist6=edgehist6, #edgehist7=edgehist7,
+
+    np.savez(NAME,x_hists = x_hists, edge_hists = edge_hists,
                                      node_label_hist=node_label_hist,
                                      x_bins = x_bins,
-                                     edgebins1=edgebins1, edgebins2=edgebins2, edgebins3=edgebins3, edgebins4=edgebins4, edgebins5=edgebins5, edgebins6=edgebins6, #edgebins7=edgebins7,
+                                     edge_bins = edge_bins,
                                      node_label_bins=node_label_bins)
     
     
-    """
-    with open("configurations/configuration.json", "r") as io:
-        cfg = json.load(io)
-    
-    #dataset,_=create_datasets(root="",div=1,cfg=cfg)
-    x1bins=np.arange(-3,17,2)
-    x2bins=np.arange(-100,37.5,12.5)
-    edgebins=np.arange(0,71.5,6.5)
-    
-    dataset=torch.load(f"processed/data_{0}.pt")
-    x1hist = get_hist(dataset.x[:,0],x1bins)
-    x2hist = get_hist(dataset.x[:,1],x2bins)
-    edgehist = get_hist(dataset.edge_attr,edgebins)
-    #print(x2hist.shape)
-    for i in range(4):
-        dataset=torch.load(f"processed/data_{i+1}.pt")
-        x1hist_temp=get_hist(dataset.x[:,0],x1bins)
-        x1hist+=x1hist_temp
-        x2hist_temp = get_hist(dataset.x[:,1],x2bins)
-        x2hist+=x2hist_temp
-        edgehist_temp = get_hist(dataset.edge_attr,edgebins)
-        edgehist+=edgehist_temp
-        print(edgehist)
-    #np.savez("data_histograms",x1hist=x1hist,x2hist=x2hist,edgehist=edgehist,x1bins=x1bins,x2bins=x2bins,edgebins=edgebins)
-    """
+   
 if NO_Va:
     x_bins=np.insert(x_bins,3,np.zeros(10),axis=0)
     x_hists=np.insert(x_hists,3,np.zeros(10),axis=0)
@@ -342,63 +290,59 @@ ax22.set_ylabel('Number of Nodes')
 fig22.savefig(path+"ac_node_feature_distr_genPMin_"+NAME+".png", bbox_inches='tight')
 
 
+if N_EDGE_FEATURES == 1:
+    fig3,ax3=plt.subplots()
+    ax3.bar(edge_bins[0],edge_hists[0],width=edge_bins[0,1]-edge_bins[0,0],align='edge', color='orange')
+    #ax3.set_title("Edge Feature Capacity")
+    ax3.set_xlabel("Admittance")
+    ax3.set_ylabel('Number of Edges')
+    fig3.savefig(path+"ac_edge_feature_Admittance_distr_"+NAME+".png", bbox_inches='tight')
+else:
 
-fig3,ax3=plt.subplots()
-ax3.bar(edgebins1,edgehist1,width=edgebins1[1]-edgebins1[0],align='edge', color='orange')
-#ax3.set_title("Edge Feature Capacity")
-ax3.set_xlabel("Capacity [MVA]")
-ax3.set_ylabel('Number of Nodes')
-fig3.savefig(path+"ac_edge_feature_capacity_distr_"+NAME+".png", bbox_inches='tight')
-
-fig4,ax4=plt.subplots()
-ax4.bar(edgebins2,edgehist2,width=edgebins2[1]-edgebins2[0],align='edge', color='orange')
-#ax4.set_title("Active PF")
-ax4.set_xlabel("Active Power Flow [MVA]")
-ax4.set_ylabel('Number of Nodes')
-fig4.savefig(path+"ac_edge_feature_active_pf_distr_"+NAME+".png", bbox_inches='tight')
-
-fig6,ax6=plt.subplots()
-ax6.bar(edgebins3,edgehist3,width=edgebins3[1]-edgebins3[0],align='edge', color='orange')
-#ax6.set_title("Edge Feature reactive PF")
-ax6.set_xlabel("Reactive Power Flow [MVA]")
-ax6.set_ylabel('Number of Nodes')
-fig6.savefig(path+"ac_edge_feature_reactive_pf_distr_"+NAME+".png", bbox_inches='tight')
-
-fig7,ax7=plt.subplots()
-ax7.bar(edgebins4,edgehist4,width=edgebins4[1]-edgebins4[0],align='edge', color='orange')
-#ax7.set_title("Edge Feature Status")
-ax7.set_xlabel("Resistance [p.U.]")
-ax7.set_ylabel('Number of Nodes')
-fig7.savefig(path+"ac_edge_feature_resistance_distr_"+NAME+".png", bbox_inches='tight')
-
-fig8,ax8=plt.subplots()
-ax8.bar(edgebins5,edgehist5,width=edgebins5[1]-edgebins5[0],align='edge', color='orange')
-#ax8.set_title("Edge Feature resistance")
-ax8.set_xlabel("Reactance [.p.U.]")
-ax8.set_ylabel('Number of Nodes')
-fig8.savefig(path+"ac_edge_feature_reactance_distr_"+NAME+".png", bbox_inches='tight')
-
-fig9,ax9=plt.subplots()
-ax9.bar(edgebins6,edgehist6, width=edgebins6[1]-edgebins6[0], align='edge', color='orange')
-#ax9.set_title("Edge Feature reactance")
-ax9.set_xlabel("Initial Damage [p.U.]")
-ax9.set_ylabel('Number of Nodes')
-fig9.savefig(path+"ac_edge_feature_init_dmg_distr_"+NAME+".png", bbox_inches='tight')
-
-"""
-fig10,ax10=plt.subplots()
-ax10.bar(edgebins7[0:10],edgehist7,width=edgebins7[1]-edgebins7[0],align='edge')
-#ax10.set_title("Edge Feature Init Damage")
-ax10.set_xlabel("Initial Damage Indicator")
-ax10.set_ylabel('Number of Nodes')
-fig10.savefig(path+"ac_edge_feature_init_dmg_distr_"+NAME+".png", bbox_inches='tight')
+    fig3,ax3=plt.subplots()
+    ax3.bar(edge_bins[0],edge_hists[0],width=edge_bins[0,1]-edge_bins[0,0],align='edge', color='orange')
+    #ax3.set_title("Edge Feature Capacity")
+    ax3.set_xlabel("Capacity [MVA]")
+    ax3.set_ylabel('Number of Nodes')
+    fig3.savefig(path+"ac_edge_feature_capacity_distr_"+NAME+".png", bbox_inches='tight')
 
 
-fig4,ax4=plt.subplots()
-ax4.bar(x3bins[0:9],x3hist,width=x3bins[1]-x3bins[0],align='edge')
-ax4.set_title("Node Feature Voltage Amplitude")
-ax4.set_xlabel("")
-fig4.savefig("ac_node_feature_distr_voltage_amplitude.png")"""
+    fig4,ax4=plt.subplots()
+    ax4.bar(edge_bins[1],edge_hists[1],width=edge_bins[1,1]-edge_bins[1,0],align='edge', color='orange')
+    #ax4.set_title("Active PF")
+    ax4.set_xlabel("Active Power Flow [MVA]")
+    ax4.set_ylabel('Number of Nodes')
+    fig4.savefig(path+"ac_edge_feature_active_pf_distr_"+NAME+".png", bbox_inches='tight')
+
+    fig6,ax6=plt.subplots()
+    ax6.bar(edge_bins[2],edge_hists[2],width=edge_bins[2,1]-edge_bins[2,0],align='edge', color='orange')
+    #ax6.set_title("Edge Feature reactive PF")
+    ax6.set_xlabel("Reactive Power Flow [MVA]")
+    ax6.set_ylabel('Number of Nodes')
+    fig6.savefig(path+"ac_edge_feature_reactive_pf_distr_"+NAME+".png", bbox_inches='tight')
+
+    fig7,ax7=plt.subplots()
+    ax7.bar(edge_bins[3],edge_hists[3],width=edge_bins[3,1]-edge_bins[3,0],align='edge', color='orange')
+    #ax7.set_title("Edge Feature Status")
+    ax7.set_xlabel("Resistance [p.U.]")
+    ax7.set_ylabel('Number of Nodes')
+    fig7.savefig(path+"ac_edge_feature_resistance_distr_"+NAME+".png", bbox_inches='tight')
+
+    fig8,ax8=plt.subplots()
+    ax8.bar(edge_bins[4],edge_hists[4],width=edge_bins[4,1]-edge_bins[4,0],align='edge', color='orange')
+    #ax8.set_title("Edge Feature resistance")
+    ax8.set_xlabel("Reactance [.p.U.]")
+    ax8.set_ylabel('Number of Nodes')
+    fig8.savefig(path+"ac_edge_feature_reactance_distr_"+NAME+".png", bbox_inches='tight')
+
+    fig9,ax9=plt.subplots()
+    ax9.bar(edge_bins[5],edge_hists[5],width=edge_bins[5,1]-edge_bins[5,0],align='edge', color='orange')
+    #ax9.set_title("Edge Feature reactance")
+    ax9.set_xlabel("Initial Damage [p.U.]")
+    ax9.set_ylabel('Number of Nodes')
+    fig9.savefig(path+"ac_edge_feature_init_dmg_distr_"+NAME+".png", bbox_inches='tight')
+
+
 
 
 fig5,ax5=plt.subplots()
