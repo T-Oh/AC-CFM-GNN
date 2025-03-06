a07bff39b4ddee4295d63ac9229e3c56ba8413cd
T-Oh <tobias_ohlinger@web.de>
2025-03-06 16:14:06 +0100

    improved logging and plotting behaviour to save more outputs correctly (f.e. train and test R2 of both node reg labels in that case)

--- src/training/training.py
+++ src/training/training.py
@@ -2,17 +2,23 @@ import torch
 import pickle
 import logging
 import time
+
+from ray.air import session
+from ray import train
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import matplotlib
+matplotlib.use('Agg')  # Use a non-GUI backend for Matplotlib
+import matplotlib.pyplot as plt
+
+
 from training.engine import Engine
 from models.get_models import get_model
 from utils.get_optimizers import get_optimizer
-
-#from ray import tune
-from ray.air import session
-
 from utils.utils import weighted_loss_label, setup_params, setup_params_from_search_space
-from models.run_node2vec import run_node2vec
 from datasets.dataset import create_datasets, create_loaders, get_attribute_sizes
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from datasets.dataset_graphlstm import create_lstm_datasets, create_lstm_dataloader
+
+
 
 
 def run_training(trainloader, testloader, engine, cfg, LRScheduler, fold = -1):
@@ -65,16 +71,12 @@ def run_training(trainloader, testloader, engine, cfg, LRScheduler, fold = -1):
             temp_eval, _, _ = engine.eval(testloader)
         LRScheduler.step(temp_eval['loss'])
 
-        #Logging
-        temp_train_loss = metrics['loss']
-        #print(f'TrainLoss: {temp_train_loss}')
-
-        result, metrics, eval = log_metrics(temp_metrics, temp_eval, metrics, eval, i, TASK, cfg['dataset::path'], SAVENAME)
+        _, metrics_, eval = log_metrics(temp_metrics, temp_eval, metrics, eval, i, TASK, cfg['cfg_path'], SAVENAME)
         t2 = time.time()
         print(f'Training Epoch took {(t2-t1)/60} mins', flush=True)
 
 
-    output = temp_output   
+    output = temp_output  
     labels = temp_labels
     #Plotting
     plotting(metrics_, eval, output, labels, 'results/plots/', '', TASK)
@@ -88,14 +90,26 @@ def objective(search_space, cfg, device,
               mask_probs, pin_memory, N_CPUS):
 
     TASK = cfg['task']
+    max_seq_length = -1
+    NAME = session.get_trial_id() if session else "unknown_trial"
 
     #Create Datasets and Loaders
-    trainset, testset = create_datasets(cfg["dataset::path"], cfg=cfg, pre_transform=None, stormsplit=cfg['stormsplit'], data_type=cfg['data'])
-    trainloader, testloader = create_loaders(cfg, trainset, testset, pin_memory=pin_memory, data_type=cfg['data'])  #, num_workers=int(N_CPUS)
+    if cfg['model'] == 'Node2Vec':
+            trainset, testset = create_datasets(cfg["dataset::path"], cfg=cfg, pre_transform=None, stormsplit=cfg['stormsplit'], data_type=cfg['data'], edge_attr=cfg['edge_attr'])
+            trainloader, testloader, max_seq_length = create_loaders(cfg, trainset, testset, Node2Vec=True)    #If Node2Vec is applied the embeddings must be calculated first which needs a trainloader with batchsize 1
+    elif cfg['model'] == 'GATLSTM':
+        # Split dataset into train and test indices
+        trainset, testset = create_lstm_datasets(cfg["dataset::path"], cfg['train_size'], cfg['manual_seed'])
+        # Create DataLoaders for train and test sets
+        trainloader = create_lstm_dataloader(trainset, batch_size=cfg['train_set::batchsize'], shuffle=True)
+        testloader = create_lstm_dataloader(testset, batch_size=cfg['test_set::batchsize'], shuffle=False)
+    else:
+        trainset, testset = create_datasets(cfg["dataset::path"], cfg=cfg, pre_transform=None, stormsplit=cfg['stormsplit'], data_type=cfg['data'], edge_attr=cfg['edge_attr'])
+        trainloader, testloader, max_seq_length = create_loaders(cfg, trainset, testset, num_workers=N_CPUS, pin_memory=pin_memory, data_type=cfg['data'], task=cfg['task'])
 
     num_features, num_edge_features, num_targets = get_attribute_sizes(cfg, trainset)
 
-    params = setup_params(cfg, mask_probs, num_features, num_edge_features, num_targets)
+    params = setup_params(cfg, mask_probs, num_features, num_edge_features, num_targets, max_seq_length)
     params = setup_params_from_search_space(search_space, params)
 
     print(params)  
@@ -110,6 +124,7 @@ def objective(search_space, cfg, device,
     model = get_model(cfg, params)
     #model = torch.compile(model_)
     model.to(device)
+
 
     #Choose Criterion
     if TASK == 'GraphClass':
@@ -140,20 +155,27 @@ def objective(search_space, cfg, device,
     start = time.time()
 
     for i in range(1, cfg['epochs'] + 1):
+        print(f'Epoch: {i}', flush=True)
         temp_metrics, output, labels = engine.train_epoch(trainloader, params['gradclip'])
 
         #report
         if i % cfg['output_freq'] == 0:
             savename = ''
             temp_eval, _, _ =  engine.eval(testloader)
-            for key in search_space.keys():
-                if key in ['num_layers', 'hidden_size', 'embedding_dim', 'walk_length', 'reghead_size', 'reghead_layers', 'K', 'num_heads',
-                        'loss_type', 'use_batchnorm', 'use_masking', 'use_skipcon']:
+            """for key in search_space.keys():
+                if key in ['num_layers', 'hidden_size', 'embedding_dim', 'walk_length', 'reghead_size', 'reghead_layers', 'K', 'num_lstm_layers',
+                        'lstm_hidden_size', 'num_conv_targets', 'loss_type', 'use_batchnorm', 'use_masking', 'use_skipcon', 'heads', ]:
                     savename = '_' + key + '_' + str(int(params[key])) + savename
                 else:
-                    savename = '_' + key + '_' + f'{params[key]:.3}'  + savename
-            result, metrics, evaluation = log_metrics(temp_metrics, temp_eval, metrics, evaluation, i, TASK, cfg['dataset::path'], savename)              
-            session.report(temp_eval)
+                    savename = '_' + key + '_' + f'{params[key]:.2}'  + savename"""
+            result, metrics, evaluation = log_metrics(temp_metrics, temp_eval, metrics, evaluation, i, TASK, cfg['cfg_path'], NAME)              
+            temp_report = { 'train_loss' : temp_metrics['loss'],
+                            'test_loss' : temp_eval['loss'],
+                            'train_R2' : temp_metrics['R2'],
+                            'test_R2' : temp_eval['R2'],
+                            'train_R2_2' : temp_metrics['R2_2'],
+                            'test_R2_2' : temp_eval['R2_2']}
+            session.report(temp_report)   
 
             torch.save(list(output), cfg['cfg_path'] + "results/" + 'output.pt') #saving train losses
             torch.save(list(labels), cfg['cfg_path'] + "results/" + 'labels.pt') #saving train losses
@@ -284,4 +306,55 @@ def log_metrics(temp_metrics, temp_eval, metrics, eval, epoch, TASK, path, saven
 
     return result, metrics, eval
         
+def plotting(metrics_, eval, output, labels, folder, NAME, task):
+    print('Plotting...')
+    for key in metrics_:
+        print('metrics')
+        fig1, ax1 = plt.subplots()
+        ax1.plot(metrics_[key], label='Train' + key)
+        ax1.plot(eval[key], label='Test' + key)
+        ax1.legend()
+
+        fig1.savefig(folder+key+'_'+NAME+'.png', bbox_inches='tight')
+
+        plt.close()
+
+    if task == 'GraphReg':
+        print('Scatter Plot, graph reg')
+        fig, ax = plt.subplots()
+        ax.scatter(labels, output, alpha=0.5)
+        ax.plot([0,1],[0,1])
+        ax.set_xlabel('Labels')
+        ax.set_ylabel('Output')
+        ax.set_title('Scatter - Graph Level')
+
+        fig.savefig( folder + 'scatter_outputVSlabel_graph_Vreal_' + NAME + '.png', bbox_inches='tight')
+
+        plt.close()
+
+    else:
+        print('Scatter Plot, node reg')
+        for i in range(int(len(output)/2000)):
+            # Scatter plot Vreal
+            fig, ax = plt.subplots()
+            ax.scatter(labels[2000*i:2000*(i+1), 0], output[2000*i:2000*(i+1), 0], alpha=0.5)
+            ax.plot([-1,1],[-1,1])
+            ax.set_xlabel('Labels')
+            ax.set_ylabel('Output')
+            ax.set_title(f'Scatter Re(V) - Chunk {i}')
+            fig.savefig(folder + f'scatter_outputVSlabel_{i}_Vreal_' + NAME+ '.png', bbox_inches='tight')
+            plt.close()
+
+            # Scatter plot Vimag
+            fig, ax = plt.subplots()
+            ax.scatter(labels[2000*i:2000*(i+1), 1], output[2000*i:2000*(i+1), 1], alpha=0.5)
+            ax.plot([-1,1],[-1,1])
+            ax.set_xlabel('Labels')
+            ax.set_ylabel('Output')
+            ax.set_title(f'Scatter Imag(V) - Chunk {i}')
+            fig.savefig(folder + f'scatter_outputVSlabel_{i}_Vimag_' + NAME + '.png', bbox_inches='tight')
+            plt.close()
+
+
+
 
