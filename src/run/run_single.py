488387008d02787df790a22112223eab8135e622
T-Oh <tobias_ohlinger@web.de>
2025-05-12 09:21:42 +0200

    cleanup of run single and streamlined output saving

--- src/run/run_single.py
+++ src/run/run_single.py
@@ -10,11 +10,10 @@ import torch
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from datasets.dataset import create_datasets, create_loaders, calc_mask_probs, get_attribute_sizes
-from datasets.dataset_graphlstm import create_lstm_datasets, create_lstm_dataloader
 from models.get_models import get_model
 from models.run_mean_baseline import run_mean_baseline
 from models.run_node2vec import run_node2vec
-from utils.utils import weighted_loss_label, setup_params, state_loss, save_params
+from utils.utils import setup_params, choose_criterion, save_params, setup_datasets_and_loaders, save_output
 from utils.get_optimizers import get_optimizer
 from training.engine import Engine
 from training.training import run_training
@@ -36,57 +35,28 @@ def run_single(cfg, device, N_CPUS):
         else:                   pin_memory = False
 
         # Create Datasets and Dataloaders
-        max_seq_length = -1
-        if cfg['model'] == 'Node2Vec':
-             trainset, testset = create_datasets(cfg["dataset::path"], cfg=cfg, pre_transform=None, stormsplit=cfg['stormsplit'], data_type=cfg['data'], edge_attr=cfg['edge_attr'])
-             trainloader, testloader, max_seq_length = create_loaders(cfg, trainset, testset, Node2Vec=True)    #If Node2Vec is applied the embeddings must be calculated first which needs a trainloader with batchsize 1
-        elif cfg['model'] == 'GATLSTM':
-            # Split dataset into train and test indices
-            trainset, testset = create_lstm_datasets(cfg["dataset::path"], cfg['train_size'], cfg['manual_seed'], 
-                                                     stormsplit=cfg['stormsplit'], max_seq_len=cfg['max_seq_length'])
-            # Create DataLoaders for train and test sets
-            trainloader = create_lstm_dataloader(trainset, batch_size=cfg['train_set::batchsize'], shuffle=True, pin_memory=pin_memory, num_workers=N_CPUS)
-            testloader = create_lstm_dataloader(testset, batch_size=cfg['test_set::batchsize'], shuffle=False, pin_memory=pin_memory, num_workers=N_CPUS)
-        else:
-             trainset, testset = create_datasets(cfg["dataset::path"], cfg=cfg, pre_transform=None, stormsplit=cfg['stormsplit'], data_type=cfg['data'], edge_attr=cfg['edge_attr'])
-             trainloader, testloader, max_seq_length = create_loaders(cfg, trainset, testset, num_workers=N_CPUS, pin_memory=pin_memory, data_type=cfg['data'], task=cfg['task'])
+        max_seq_len_LDTSF, trainset, trainloader, testloader = setup_datasets_and_loaders(cfg, N_CPUS, pin_memory)
 
         # Calculate probabilities for masking of nodes if necessary
         mask_probs = calc_mask_probs(trainloader, cfg)
-
 
         # getting feature sizes if datatype is not LDTSF
         num_features, num_edge_features, num_targets = get_attribute_sizes(cfg, trainset)
 
         #Setup Parameter dictionary for Node2Vec (mask_probs, num_features and num_edge_features should be irrelevant)
-        params = setup_params(cfg, mask_probs, num_features, num_edge_features, num_targets, max_seq_length, save=True)
+        params = setup_params(cfg, mask_probs, num_features, num_edge_features, num_targets, max_seq_len_LDTSF)
         save_params(cfg['cfg_path'], params, 'single')
 
         #Node2Vec
         if cfg['model'] == 'Node2Vec':  trainloader, testloader, params = setup_node2vec(cfg, device, trainloader, mask_probs, params)
 
-
-        #Regular Models (GINE, GAT, TAG, MLP, GraphTransformer)
         # Init Criterion
-        if cfg['task'] in ['GraphClass', 'typeIIClass']:
-            criterion = torch.nn.CrossEntropyLoss()
-        elif cfg['weighted_loss_label']:
-            criterion = weighted_loss_label(
-            factor=torch.tensor(cfg['weighted_loss_factor']))
-        elif cfg['task'] == 'StateReg':
-            criterion = state_loss(cfg['weighted_loss_factor'])
-        else:
-            criterion = torch.nn.MSELoss(reduction='mean')  # TO defines the loss
-
-        #criterion.to(device)
+        criterion = choose_criterion(cfg['task'], cfg['weighted_loss_label'], cfg['weighted_loss_factor'])
 
         # Loading GNN model
-        model_ = get_model(cfg, params)
-        model = model_  #torch.compile(model_)
-        model.to(device)
-        pytorch_total_params = sum(p.numel() for p in model.parameters())
-        print('NUMBER OF PARAMETERS:')
-        print(pytorch_total_params)
+        model = get_model(cfg, params)
+        #model = model_  #torch.compile(model_)
+        #model.to(device)
 
         # Init optimizer
         optimizer = get_optimizer(cfg, model, params)
@@ -101,15 +71,11 @@ def run_single(cfg, device, N_CPUS):
         #Run Training
         _, _, output, labels, test_output, test_labels = run_training(trainloader, testloader, engine, cfg, LRScheduler)
 
-        #Save outputs, labels and losses of first fold
-        torch.save(output, "results/" + "output.pt")  # saving train losses
-        torch.save(labels, "results/" + "labels.pt")  # saving train losses
-        torch.save(output, "results/" + "test_output.pt")  # saving train losses
-        torch.save(labels, "results/" + "test_labels.pt")  # saving train losses
-        #torch.save(list(metrics['train_loss']), "results/" + "train_losses.pt")  # saving train losses
-        #torch.save(list(metrics['test_loss']), "results/" + "test_losses.pt")  # saving train losses
-    
+
+        save_output(output, labels, test_output, test_labels)
+        #Save Model
         torch.save(model.state_dict(), "results/" + cfg["model"] + ".pt")
+
 
 
 
